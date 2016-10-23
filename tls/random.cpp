9126dac96f8019636504603a1fd454a6be8b600b
Jorrit Stramer <jstramer@student.tugraz.at>
2016-10-23 15:08:49 +0200

    implemented first version of the random generator

--- tls/random.cpp
+++ tls/random.cpp
@@ -25,8 +25,24 @@
 ******************************************************************************/
 
 #include "random.h"
+#include <fstream>
 
 void get_random_data(uint8_t* data, std::size_t size)
 {
-  /// \todo Fetch size random bytes and store them in data.
+  /*
+   * NOTE: srand(time(0)) not secure because time is used as seed
+   *       If attacker can guess the time -> easily crackable
+   *       rand() implementations also platform specific and not secure
+   *
+   * /dev/random more secure but it blocks when entropy pool is empty,
+   * /dev/urandom does not block, therefore it is theoretically vulnerable
+   * to cryptoanalysis, but creates enough randomness for this task
+  */
+
+  std::ifstream random("/dev/urandom", std::ios::in | std::ios::binary);
+  if (random.is_open())
+  {
+    random.read(reinterpret_cast<char*>(data), size);
+    random.close();
+  }
 }
