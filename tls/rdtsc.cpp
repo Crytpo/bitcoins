8c978ba00b816cfabc131eb55fcb9ecb6211f30e
Jorrit Stramer <jstramer@student.tugraz.at>
2016-12-04 16:41:24 +0100

    implemented the local attack and changed RDTSC

--- tls/rdtsc.cpp
+++ tls/rdtsc.cpp
@@ -27,64 +27,30 @@
 #include <iostream>
 #include "clocks.h"
 
-using namespace std;
-
-
-bool checkCpuId()
-{
-  uint32_t reg[4]; // "GenuineIntel", must be written which can do an intel cpu.
-  __asm__ __volatile__(
-                       "cpuid" // is a serializing call
-                       : "=a"(reg[3]), "=b"(reg[0]), "=c"(reg[2]), "=d"(reg[1])
-                       : "a"(0)
-                       : "cc"
-                       );
-
-  if ((char*)reg != (char*)"GenuineIntel")
-  {
-    cout << reg << endl;
-    cout << "Error: cpu does not provide timestampcounter!" << endl;
-    
-    return false;
-  }
-  
-  return true;
-}
-
 uint64_t rdtsc_clock::begin()
 {
-  /// \todo Implement start of RDTSC based measurement here.
-  // https://www.ccsl.carleton.ca/~jamuir/rdtscpm1.pdf
-  // https://www.lmax.com/blog/staff-blogs/2015/10/25/time-stamp-counters/
-  // http://stackoverflow.com/questions/12631856/difference-between-rdtscp-rdtsc-memory-and-cpuid-rdtsc
-  uint32_t hi = 0;
-  uint32_t lo = 0;
-  
-  if( not checkCpuId())
-    return 0;
-  
-  __asm__ __volatile__(
-                       "rdtsc" // waits until previous instructions have been executed
-                       : "=a"(lo), "=d"(hi) 
-                      );
+  uint32_t cycles_high = 0;
+  uint32_t cycles_low = 0;
 
-  return ( ((uint64_t) lo) | (((uint64_t) hi) << 32) );
+  // most modern CPUs should support RDTSCP, therefore that should be enough
+  // no need to use CPUID for synchronization + RDTSC
+  __asm__ __volatile__(
+        "rdtscp"
+          : "=d"(cycles_high), "=a"(cycles_low)
+  );
+
+  return ( (static_cast<uint64_t>(cycles_high) << 32) | static_cast<uint64_t>(cycles_low));
 }
 
 uint64_t rdtsc_clock::end()
 {
-  /// \todo Implement end of RDTSC based measurement here.
-  // https://github.com/prashrock/C/blob/master/micro_benchmarks/rdtsc.c
-  uint32_t hi = 0;
-  uint32_t lo = 0;
+  uint32_t cycles_high = 0;
+  uint32_t cycles_low = 0;
   
   __asm__ __volatile__(
-                       "rdtscp"
-                       : "=a"(lo), "=d"(hi)
-                      );
+        "rdtscp"
+          : "=d"(cycles_high), "=a"(cycles_low)
+  );
   
-  if (not checkCpuId())
-    return 0;
-  
-  return ( ((uint64_t) lo) | (((uint64_t) hi) << 32) );
+  return ( (static_cast<uint64_t>(cycles_high) << 32) | static_cast<uint64_t>(cycles_low));
 }
